fb68e63aeee2618cf06092f7492367e6e410da5a
Vincente Pericoli <vspericoli@ucdavis.edu>
2015-12-01 20:56:21 -0800

    compatibility fix

--- FEM_VGPy/myPaths.py
+++ FEM_VGPy/myPaths.py
@@ -2,12 +2,12 @@
 
 def PyMATLAB():
     """ returns the path to the 64bit matlab engine """
-    return "C:\Users\Vince Pericoli\Documents\MATLAB\PyEngine\Lib\site-packages"
+    return "C:\\Users\\Vince Pericoli\\Documents\\MATLAB\\PyEngine\\Lib\\site-packages"
 
 def OdbTools():
     """ returns the path to abaqus-odb-tools """
-    return "C:\Users\Vince Pericoli\Documents\GitHub\abaqus-odb-tools"
+    return "C:\\Users\\Vince Pericoli\\Documents\\GitHub\\abaqus-odb-tools"
 
 def saveResults():
     """ returns the path to the VGPy save location """
-    return "C:\Temp\VGPy_Databases"
+    return "C:\\Temp\\VGPy_Databases"
