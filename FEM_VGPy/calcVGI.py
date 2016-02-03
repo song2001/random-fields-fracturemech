f711b50de5b53a0ad9e29b6fbc6e10ea53468fc5
Vincente Pericoli <vspericoli@ucdavis.edu>
2016-02-03 10:38:13 -0800

    update to data structure & mem. management

--- FEM_VGPy/calcVGI.py
+++ FEM_VGPy/calcVGI.py
@@ -23,12 +23,13 @@ def __check_input_args(mises, pressure, PEEQ):
 
 def calcMonotonicVGI(mises, pressure, PEEQ):
     """
-    Takes rank-2 matrices of mises, pressure, PEEQ
-    returns a rank-2 matrix of monotonic VGI
+    Takes matrices of mises, pressure, PEEQ
+    returns a matrix of monotonic VGI
     
     matrix must be ordered such that the rows are different
-    "history" values, and the columns are different "nodes"
-    or other such distinctly different objects
+    "history" values, and the columns (or other dimensions,
+    for rank-3 and higher arrays) are different "nodes"
+    or other such distinctly different objects.
     
     Verified to produce accurate results: 09/21/2015
     """
@@ -42,7 +43,7 @@ def calcMonotonicVGI(mises, pressure, PEEQ):
     # calculate the stress triaxiality
     # stress triaxiality is an element-wise divide of -pressure/mises
     # skip first element (since division by zero), this will remain 0
-    triax[1:,:] = -pressure[1:,:]/mises[1:,:]
+    triax[1:] = -pressure[1:]/mises[1:]
     
     # calculate the integrand
     integrand = numpy.exp(1.5*triax)
@@ -50,22 +51,17 @@ def calcMonotonicVGI(mises, pressure, PEEQ):
     # preallocate
     VGI  = numpy.zeros(mises.shape, dtype=numpy.float64)
     nrow = mises.shape[0]
-    ncol = mises.shape[1]
     
     # trap rule numerical integration:
     for row in range(1,nrow):
         # for all rows ("history" values), except first row
-        for col in range(0,ncol):
-            # for all columns (or "objects" with distinctly different VGIs)
             
-            # incremental VGI
-            dVGI = 0.5*(PEEQ[row,col] - PEEQ[row-1,col])*(integrand[row,col] + integrand[row-1,col])
-            
-            # sum into VGI
-            VGI[row,col] = VGI[row-1,col] + dVGI
-
+        # incremental VGI
+        dVGI = 0.5*(PEEQ[row] - PEEQ[row-1])*(integrand[row] + integrand[row-1])
+        
+        # sum into VGI
+        VGI[row] = VGI[row-1] + dVGI
     return VGI
-
     
 def calcCyclicVGI(mises, pressure, PEEQ):
     """
